a8a691c7fe004c5bb32ab94d69309fdfb1db0a1f
wickramasr <wickramasr@webster.ac.th>
2020-03-29 17:16:32 +0700

    Create Tables

---
+++ install.sql
@@ -0,0 +1,77 @@
+CREATE TABLE Customer
+(
+  Cus_Name INT NOT NULL,
+  Cus__user_ID INT NOT NULL,
+  Cus_phone_number INT NOT NULL,
+  Cus_Address INT NOT NULL,
+  Cus_Password INT NOT NULL,
+  PRIMARY KEY (Cus__user_ID)
+);
+
+CREATE TABLE Recipe
+(
+  Recipe_Name INT NOT NULL,
+  Recipe_ID INT NOT NULL,
+  Recipe_Type INT NOT NULL,
+  Description INT NOT NULL,
+  Preparing_Time INT NOT NULL,
+  PRIMARY KEY (Ingredient_ID, Recipe_ID)
+);
+
+CREATE TABLE Ingredient
+(
+  Ingredient_name INT NOT NULL,
+  Ingredient_ID INT NOT NULL,
+  Ingredient_Amount INT NOT NULL,
+  PRIMARY KEY (Ingredient_ID)
+);
+
+CREATE TABLE Bill
+(
+  Ingredient_Order INT NOT NULL,
+  Bill_ID INT NOT NULL,
+  Date_and_Time INT NOT NULL,
+  PRIMARY KEY (Bill_ID)
+);
+
+CREATE TABLE Searched_By
+(
+  Cus__user_ID INT NOT NULL,
+  Ingredient_ID INT NOT NULL,
+  Recipe_ID INT NOT NULL,
+  FOREIGN KEY (Cus__user_ID) REFERENCES Customer(Cus__user_ID),
+  FOREIGN KEY (Ingredient_ID, Recipe_ID) REFERENCES Recipe(Ingredient_ID, Recipe_ID)
+);
+
+CREATE TABLE Is_paid_by
+(
+  Cus__user_ID INT NOT NULL,
+  Bill_ID INT NOT NULL,
+  FOREIGN KEY (Cus__user_ID) REFERENCES Customer(Cus__user_ID),
+  FOREIGN KEY (Bill_ID) REFERENCES Bill(Bill_ID)
+);
+
+CREATE TABLE Is_in
+(
+  Ingredient_ID INT NOT NULL,
+  Recipe_ID INT NOT NULL,
+  FOREIGN KEY (Ingredient_ID) REFERENCES Ingredient(Ingredient_ID),
+  FOREIGN KEY (Ingredient_ID, Recipe_ID) REFERENCES Recipe(Ingredient_ID, Recipe_ID)
+);
+
+CREATE TABLE Are_mentioned_in
+(
+  Bill_ID INT NOT NULL,
+  Ingredient_ID INT NOT NULL,
+  FOREIGN KEY (Bill_ID) REFERENCES Bill(Bill_ID),
+  FOREIGN KEY (Ingredient_ID) REFERENCES Ingredient(Ingredient_ID)
+);
+
+insert into Customer values("Sahan", 4160206,0622394802,"Sathorn","abcd")
+insert into Recipe values("Biriyani",111,"aaa","tasty Dish",60)
+insert into Ingredient values("Rice",112,"1kg")
+insert into Bill values("Rice",2020-03-29,60)
+insert into Searched_By values(11111,112,111)
+insert into Is_paid_bt values(4160206,13456)
+insert into Is_in values(112,111)
+insert into Are_mentioned_in values(12344,112)
