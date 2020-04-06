3ceb9c00adbf02ac2a85ee554142b4fb1ee73ef9
wickramasr <53009224+wickramasr@users.noreply.github.com>
2020-04-06 17:07:02 +0700

    Updated target_nepsri

--- target_nepsri.php
+++ target_nepsri.php
@@ -6,19 +6,17 @@ $plocation = $_POST['Description'];
 $dnum = $_POST['Preparing_Time'];
 $id = mt_rand(10000, 99999); 
 
-$servername = "localhost";
-$username = "root";
-$password = "rashmika1996";
-$dbname = "nepsri";
+include 'connect.php';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+ die("Connection failed: " . $conn->connect_error);
 }
- $sql = "insert into recipe values('$pname', '$id', '$pnumber', '$plocation' ,'$dnum');";
-$result = $conn->query($sql);
+ 	
+	$sql = "insert into recipe values('$pname', '$id', '$pnumber', '$plocation' ,'$dnum');";
+	$result = $conn->query($sql);
 
         header ("Location: nepsri.php");
 ?>
