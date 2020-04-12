b02236fa7cf0347e85faa1344ad0b1dfd21e48d1
wickramasr <53009224+wickramasr@users.noreply.github.com>
2020-04-12 22:32:09 +0700

    target_ingredient.php

---
+++ target_ingredient.php
@@ -0,0 +1,21 @@
+<?php
+
+$iname = $_POST['Ingredient_Name'];
+$inumber = $_POST['Ingredient_ID'];
+$iamount= $_POST['Ingredient_Amount'];
+
+include 'connect.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
+ 	
+	$sql = "insert into ingredient values('$iname', '$inumber', '$iamount');";
+	$result = $conn->query($sql);
+
+        header ("Location: ingredient.php");
+?>
+
