17b5ca829e602b8621cd3ac44a81d479179c7de9
wickramasr <53009224+wickramasr@users.noreply.github.com>
2020-04-19 15:50:12 +0700

    Updated index.php

--- index.php
+++ index.php
@@ -1,126 +1,141 @@
 <?php
-if(isset($_POST["Add_new_recipe"]))
-{
-header('location:Add_new_recipe.php');
+$version="";
+$server_name="";
+$server_port="";
+
+ if (getenv('HOME') == ''){                       // Not set when running as service
+   $root= substr($_SERVER["DOCUMENT_ROOT"],0,-4); // this alternative with limitations
+ }                                                // gets path to folder UniServerZ
+
+ else{                                            // Set when run as standard program
+   $root= getenv('HOME');                         // this is the ideal method to
+ }                                                // get the path to folder UniServerZ
+
+$file="$root\home\us_config\us_config.ini" ;     // Name and path of configuration file
+
+if (file_exists($file) && is_readable($file)){   // Check file
+  $settings=parse_ini_file($file,true);          // parse file into an array
+  $version=$settings["APP"]["AppVersion"];       // get parameter
+}
+
+
+$file="$root\home\us_config\us_user.ini" ;       // Name and path of user configuration file
+
+if (file_exists($file) && is_readable($file)){     // Check file
+  $settings=parse_ini_file($file,true);            // parse file into an array
+  $server_name=$settings["USER"]["US_SERVERNAME"]; // get parameter
+  $server_port=$settings["USER"]["AP_PORT"];       // get parameter
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>NepSri</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>Uniform Server ZeroXIII - test page</title>
+<meta name="Description" content="The Uniform Server ZeroXIII 13.3.1" />
+<meta name="Keywords" content="The Uniform Server, MPG, Mike Gleaves, Ric, UniServer, Olajide, BobS " />
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 </head>
 
-<style>
-table, th, td {
-  border: 2px solid black;
-  border-spacing: 1px;
-  }
-  th, td {
-  padding: 5px;
+<style type="text/css">
+/*****************************************/
+.intro{
+  margin-top:10px;
+  margin-bottom:10px;
+  padding:3px;
+  font-size:11px;
+  font-family:Verdana;
+  background-color: #E7E7FD;
+  border-top:1px solid #4f4f97;
+  border-bottom:1px solid #4f4f97;
 }
-th {
-  text-align: left;
-}
-table{
-  width: 80%;    
-  background-color: white ;
-  
-}
-table {
-  border-spacing: 2px;
-}
+/*****************************************/
 </style>
-</head>
 
 <body>
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">NepSri</a>
-    </div>
-    <ul class="nav navbar-nav">
-      
-      
-      <li class="active"><a href="#"><b>Recipe</b></a></li>
-      <li><a href="#"><b>Customer</b></a></li>
-      <li><a href="#"><b>Ingredient</b></a></li>
-      <li><a href="#"><b>Bill</b></a></li>
-      <li><a href="#"><b>About Us</b></a></li>
-        <li><a href="https://docs.google.com/document/d/1-DUma1qz0rY8BqHkYd2Lrz6LSKUHfq70c7sqaTGjUcY/edit"><strong><font        color=#2F4F2F><i         class="fa fa-fw fa-       question-circle"></i> HELP           </font></strong></a></li>
-    </ul>
-    
-     <div class="text-right"> 
-     <div class="btn">
-      <form name ="form1" action="" method="post">
-      <input type ="submit" name ="Add_new_recipe" value ="Add_new_recipe">
-     </form>
-      </div> 
-    
+<div id="wrap">
+  <div id="header">
+     <a href="http://www.uniformserver.com"><img src="images/logo.png" align="left" alt="The Uniform Server" /></a>
+       <div id="header_txt" >
+         <div style="position:absolute;">
+             ZeroXIII <?php print "- ".$version; ?> </p>
+         </div>
+       </div>
   </div>
-</nav>
-  
-<div class="container">
-  <h1><center><b>Welcome to NepSri</b></center></h1>
-
-</div>
-<?php
-
-require('connect.php');
-
-$result = $conn->query("select * from mytable");
 
 
-$sql = "SELECT * from recipe;  ";
-$result = $conn->query($sql);
-if ($result->num_rows > 0)
- 
-{
-echo '<div class="tab-content">';
-echo '<div id="donor" class="tab-pane fade in active">';
-echo '<table table-hover class="centerTable">';
-echo '<tr bgcolor=#00FFFF style="color:#00006a">
+  <div id="content">
+    <h1>Welcome to The Uniform Server</h1>
 
-<h2 style="color:Black"; align="center" class="w3-myfont">Recipe</h2>
-<br/>
-<th>Recipe Name</th>
+    <p class="intro">This test page <b>index.php</b> was served from root folder UniServerZ\<b>www</b>
+    <span  style='display:<?php print("none")?>'><br /> No PHP module installed Apache returns php directives un-processed.</span>
+    </p>
 
-<th>Recipe ID</th>
-<th>Description</th>
-<th>Preparing Time(min)</th>
+  <div align="center" >
+     <img src="images/padlock2.gif"  alt="Padlock" />
+  </div>
 
-</tr>';
+  <p class="intro"><b><i>Note</i>:</b> Please read manual page: <a href="/us_docs/manual/quick_start_guide.html#Installing your Website or Test pages">Installing your Website or Test pages</a>.</p>
 
 
-// output data of each row
+<!-- splash page link -->
+<!-- <?php print("--" . ">");?>
 
-    while($row = $result->fetch_assoc())
- {
+  <table>
+  <tr>
+   <td>
+     <h2>Server links</h2>
+      <p> <a href="http://<?php echo($server_name.':'.$server_port) ?>/us_splash/index.php" target="_blank" >Splash page</a> - Displays server specification and useful links.</p>
+      <p> <a href="http://<?php echo($server_name.':'.$server_port) ?>/us_opt1/index.php" target="_blank" >PhpMyAdmin</a>.</p>
+      <p> <a href="http://<?php echo($server_name.':'.$server_port) ?>/us_opt2/?username=" target="_blank" >Adminer</a>.</p>
+      <p> <a href="http://<?php echo($server_name.':'.$server_port) ?>/us_extra/phpinfo.php" target="_blank" >PHP Info</a>.</p>
+   </td>
+  </tr>
+  </table>
+<?php print("<"."!"."--")?> -->
 
-echo "<tr>
-<td>" . $row["Recipe_Name"]. "</td>
+<!-- subdirs  -->
+<!-- <?php print("--" . ">");?>
 
-<td>" . $row["Recipe_ID"]. "</td>
-<td>" . $row["Description"]. "</td>
-<td>" . $row["Preparing_Time"]. "</td>
-
-</tr>";
-
+  <table>
+  <tr><td><h2>Served Subdirectories</h2></td></tr>
+  </table>
+  <table width=100%>
+  <?php $n = 0; foreach (scandir("./") as $file){
+    if (is_dir($file) && !in_array($file, array(".", "..", "css", "images"))){
+        $n++;
+        echo ($n % 3 ? (($n+1) % 3 ? "<tr><td width=33%>$n - <a href='" . $file . "' target='_blank'>" . $file . "</a></td>" : "<td width=33%>$n - <a href='" . $file . "' target='_blank'>" . $file . "</a></td>") : "<td>$n - <a href='" . $file . "' target='_blank'>" . $file . "</a></td></tr>");
     }
- echo "</table>";
-} else {
-    echo "0 results";
-}  
-$conn->close();
-?>
-</body>
-</html>
+  }
+  echo ($n == 0 ? "<tr><td style='color: red;' colspan='3'>None</td></tr>" : ($n % 2 == 0 ? "" : "<td></td></tr>"));?>
+  </table>
+<?php print("<"."!"."--")?> -->
 
+
+<!-- php files  -->
+<!-- <?php print("--" . ">");?>
+
+  <table>
+  <tr><td><h2>Served PHP Files</h2></td></tr>
+  </table>
+  <table width=100%>
+
+  <?php $n = 0; foreach (scandir("./") as $file){
+    if (strtolower(strrchr($file, '.'))==".php" && $file!="index.php"){
+        $n++;
+        echo ($n % 3 ? (($n+1) % 3 ? "<tr><td width=33%>$n - <a href='" . $file . "' target='_blank'>" . $file . "</a></td>" : "<td width=33%>$n - <a href='" . $file . "' target='_blank'>" . $file . "</a></td>") : "<td>$n - <a href='" . $file . "' target='_blank'>" . $file . "</a></td></tr>");
+    }
+  }
+  echo ($n == 0 ? "<tr><td style='color: red;' colspan='3'>None</td></tr>" : ($n % 2 == 0 ? "" : "<td></td></tr>"));?>
+  </table>
+ 
+<?php print("<"."!"."--")?> -->
+
+
+  <div id="divider">Developed By <a href="http://www.uniformserver.com/">The Uniform Server Development Team</a><br>Updated By <a href="https://github.com/BrainStormDevel" target="_blank">BrainStorm</a></div>
+</div>
+</div>
 </body>
 </html>
